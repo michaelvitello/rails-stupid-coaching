92250527a19e6cc8ac7939ea30d74403afc28c43
michael vitello <michael.vitello@gmail.com>
2018-04-09 15:28:09 +1000

    Stupid Coach in Rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+get 'answer', to: 'questions#answer', as: :answer
+
+get 'ask', to: 'questions#ask', as: :ask
+
 end
